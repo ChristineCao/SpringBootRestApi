d899d2ddad1a4f6d6e8ae3adbf31b29efa59520d
ChristineCao <christine.cao.ma@gmail.com>
2017-11-01 20:34:52 -0400

    topic sevice rest api code

---
+++ src/main/java/com/christine/springboot/restapi/ClassApiApp.java
@@ -0,0 +1,12 @@
+package com.christine.springboot.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassApiApp {
+    public static void main( String[] args )
+    {	
+    	SpringApplication.run(ClassApiApp.class, args);
+    }
+}
